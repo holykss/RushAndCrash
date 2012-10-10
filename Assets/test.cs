35ef0d900a896aa50cd43a1f0c5a497ea568c1c3
Yoo Young Wook <pusan1006@nate.com>
2012-10-10 11:30:48 +0900

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class test : MonoBehaviour {
 	
-	Vector3 offset;
 	
-	void OnMouseDown()
-	{
-		Debug.Log("다운 들어왔다");
-		offset = Camera.mainCamera.transform.position - Camera.mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, Input.mousePosition.z));
-	}
-	void OnMouseDrag()
-	{
-		Vector3 curScreenPosition = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.z);
-		Vector3 curPosition = Camera.mainCamera.ScreenToWorldPoint(curScreenPosition) + offset;
-		
-		Camera.mainCamera.transform.position = curPosition;
-	}
+	
+void OnMouseDown()
+{
+  //  screenPoint = Camera.main.WorldToScreenPoint(scanPos);
+
+  //  offset = scanPos - Camera.main.ScreenToWorldPoint(
+    //    new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		Debug.Log("aaaaa");
+}
+
+
+void OnMouseDrag()
+{
+   // Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+   // Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+   // Camera.main.transform.position = curPosition;
+		Debug.Log("bbbbb");
+}
 }
