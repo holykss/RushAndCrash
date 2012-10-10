1ae6f6bf39ca08d9ca10c810d81fbca9b1ff1980
Yoo Young Wook <pusan1006@nate.com>
2012-10-10 13:27:03 +0900

    camera zoom, drag

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Global : MonoBehaviour {
 
-	public GameObject spartanKing;
+	public GameObject spartanKing = null;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +12,8 @@ public class Global : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1"))
+		
+		if (spartanKing != null && Input.GetButtonDown("Fire1"))
 		{
 			Ray ray = Camera.mainCamera.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hitInfo = new RaycastHit();
@@ -22,7 +23,33 @@ public class Global : MonoBehaviour {
 				Instantiate(spartanKing, hitInfo.point, transform.rotation);
 			}
 		}
+		
+		input();
 	
+	}
+	
+	void input()
+	{
+		
+		//camera zoom
+		if(Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			Camera.main.fieldOfView++;
+		}
+		else if(Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			Camera.main.fieldOfView--;
+		}
+		
+		//camara drag
+		bool mouseLButton = Input.GetMouseButton(0);
+		
+		if(mouseLButton)
+		{
+			Vector3 position = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+			Debug.Log("position : " + position.ToString());
+			Camera.main.transform.position -= position;
+		}
 	}
 
 	
